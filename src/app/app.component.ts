297e6152a10385f871601b09516e33fc43fe54f0
clayman33 <christianlayman@hotmail.com>
2022-06-27 16:09:22 -0500

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,27 +11,33 @@ export class AppComponent  implements OnInit{
   name = 'Angular ' + VERSION.major;
 
   ngOnInit(){
-    of('String 1', 'String 2', 'String 3').subscribe((item) => console.log(item));
+    /*of('String 1', 'String 2', 'String 3').subscribe((item) => console.log(item));*/
 
-    from(['String 1', 'String 2', 'String 3']).pipe(
-      tap(item => console.log(item))
-    )   
-    .subscribe({
+    from([5, 10, 15, 20]).pipe(
+      tap(item => console.log(item)),
+      map(item => item * 2),
+      map(item => {
+        if(item === 20) {
+          throw new Error('This is the fourth item');
+        }
+        return item;
+      })      
+    ).subscribe({
       next: (item) => console.log(`string emitted ${item}`),
       error: (err) => console.log(err),
-      complete: () => console.log('complete')
+      complete: () => console.log('This emission has been completed')
     });
 
-    of('String 1', 'string 2', 'string 3').subscribe({
+    /*of('String 1', 'string 2', 'string 3').subscribe({
       next: (item) => console.log(`string emitted using from ${item}`),
       error: (err) => console.log(`an error occurred ${err}`),
       complete: () => console.log('it completed')
-    });
+    });*/
 
-    of(2, 4, 6).pipe(
+    /*of(2, 4, 6).pipe(
       map(item => item * 2),
       tap(item => console.log(item)),
       take(2)
-    ).subscribe(item => console.log(item));
+    ).subscribe(item => console.log(item));*/
   }
 }
